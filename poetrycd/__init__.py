d610fca3d9685d0374d906084a6f5e117e2248dc
David Gilman <dgilman@atd-us.com>
2021-09-21 15:54:14 -0400

    more debug

--- poetrycd/__init__.py
+++ poetrycd/__init__.py
@@ -5,6 +5,4 @@ import subprocess
 def poetrycd():
     for filename in sys.argv[1:]:
         dirname = os.path.dirname(filename)
-        print(filename, dirname)
-        raise Exception()
-        subprocess.run("echo pwd; poetry run sync-requirements", cwd=dirname, shell=True, check=True, env=os.environ)
+        subprocess.run("poetry run sync-requirements", cwd=dirname, shell=True, check=True, env=os.environ)
