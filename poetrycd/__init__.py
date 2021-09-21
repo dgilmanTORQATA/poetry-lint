5d647b7eb1c41a4b71167ee02a01f642c622a33b
David Gilman <dgilman@atd-us.com>
2021-09-21 15:45:56 -0400

    Use subprocess with explicit cwd

--- poetrycd/__init__.py
+++ poetrycd/__init__.py
@@ -1,8 +1,8 @@
 import sys
 import os
+import subprocess
 
 def poetrycd():
     for filename in sys.argv[1:]:
         dirname = os.path.dirname(filename)
-        os.chdir(dirname)
-        os.system("poetry run sync-requirements")
+        subprocess.run(["poetry", "run", "sync-requirements"], cwd=dirname, check=True)
