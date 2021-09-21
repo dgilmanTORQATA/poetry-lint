2f191f16a72fb46d615205a522a9642214a9b719
David Gilman <dgilman@atd-us.com>
2021-09-21 15:46:56 -0400

    printf

--- poetrycd/__init__.py
+++ poetrycd/__init__.py
@@ -5,4 +5,5 @@ import subprocess
 def poetrycd():
     for filename in sys.argv[1:]:
         dirname = os.path.dirname(filename)
+        print(filename, dirname)
         subprocess.run(["poetry", "run", "sync-requirements"], cwd=dirname, check=True)
